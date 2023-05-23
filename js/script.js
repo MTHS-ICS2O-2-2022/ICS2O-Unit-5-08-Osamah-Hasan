a5173e6f426766bb808c49ae415372d8e06d73b1
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-05-23 17:21:20 +0000

    major update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah hasan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //this function divides using a loop
+  //input
+  let number1 = parseInt(document.getElementById("number-1").value);
+  let number2 = parseInt(document.getElementById("number-2").value);
+
+  //process
+  let counter = 0;
+  while (number1 >= number2) {
+    number1 -= number2;
+    counter++
+  }
+
+  //output
+  if (number1 != 0) {
+    document.getElementById('answer').innerHTML = "the answer is" + counter + "with a remainder of" + number1;
+  } else {
+    document.getElementById('answer').innerHTML = "the answer is" + counter;
+  }
+}
